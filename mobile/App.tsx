9d56d3d5db99167e174b82fbd266008d8db60093
Adler Kesley <adlerbassk@gmail.com>
2020-06-17 03:19:59 -0300

    Readme Assets folder

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -7,7 +7,7 @@ import { Ubuntu_700Bold, useFonts } from '@expo-google-fonts/ubuntu'
 import Routes from "./src/routes";
 
 
-// console.disableYellowBox = true; apenas para testes do select box.
+console.disableYellowBox = true; 
 
 export default function App() {
   const [fontsLoaded] = useFonts({
